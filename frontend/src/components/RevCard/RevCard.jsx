236fe03a978e6015cb2256379f135ca6a5c68875
Jay9874 <jayprakashsharma225@gmail.com>
2023-04-28 13:08:30 +0530

    complete review

--- frontend/src/components/RevCard/RevCard.jsx
+++ frontend/src/components/RevCard/RevCard.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import './revcard.css'
 
-export default function RevCard() {
+export default function RevCard ({ id, name, review, rating }) {
   return (
-    <div>RevCard</div>
+    <div className={`review-card`}>
+      <div className='review'>
+        <h1>{name}</h1>
+        <h2>⭐️</h2>
+        <p>{review}</p>
+      </div>
+    </div>
   )
 }
--- frontend/src/components/Review/Review.jsx
+++ frontend/src/components/Review/Review.jsx
@@ -3,6 +3,38 @@ import './review.css'
 import RevCard from '../RevCard/RevCard'
 
 export default function Review () {
+  const [reviews, setReviews] = React.useState([
+    {
+      id: 1,
+      name: 'Shubham',
+      review:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies ultrices, nunc nisl ultricies nunc, nec ultricies nisl nunc eget nisl. Sed vitae nisl eget nisl ultricies ultrices. Sed vitae nisl eget nisl ultricies ultrices.',
+      rating: 5
+    },
+    {
+      id: 3,
+      name: 'Kirti',
+      review:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies ultrices, nunc nisl ultricies nunc, nec ultricies nisl nunc eget nisl. Sed vitae nisl eget nisl ultricies ultrices. Sed vitae nisl eget nisl ultricies ultrices.',
+      rating: 3
+    },
+    {
+      id: 4,
+      name: 'Jay',
+      review:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies ultrices, nunc nisl ultricies nunc, nec ultricies nisl nunc eget nisl. Sed vitae nisl eget nisl ultricies ultrices. Sed vitae nisl eget nisl ultricies ultrices.',
+      rating: 4
+    }
+  ])
+
+  React.useEffect(() => {
+    fetch('http://localhost:5000/reviews')
+      .then(res => res.json())
+      .then(data => {
+        setReviews(data)
+      })
+  }, [])
+
   return (
     <div className='review-section'>
       <div className='review-container'>
@@ -10,7 +42,23 @@ export default function Review () {
           <h1>Our Reviews</h1>
         </div>
         <div className='review-card-container'>
-            <RevCard />
+          <div className='review-edge-overlay review-left-edge' />
+          <div className='review-edge-overlay review-right-edge' />
+          {reviews.length !== 0 ? (
+            reviews.map(review => {
+              return (
+                <RevCard
+                  key={review.id}
+                  id={review.id}
+                  name={review.name}
+                  review={review.review}
+                  rating={review.rating}
+                />
+              )
+            })
+          ) : (
+            <h1>No reviews yet</h1>
+          )}
         </div>
       </div>
     </div>
