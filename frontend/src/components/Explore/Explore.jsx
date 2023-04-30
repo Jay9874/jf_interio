1af81f56b1914484ce151bade2d0d32b9838a014
Jay9874 <jayprakashsharma225@gmail.com>
2023-04-30 22:13:42 +0530

    mobile view explore

--- frontend/src/components/Explore/Explore.jsx
+++ frontend/src/components/Explore/Explore.jsx
@@ -5,16 +5,16 @@ import { explore } from '../../assets'
 export default function Explore () {
   return (
     <div className='explore-section'>
-      <div className='explore-container section-padding'>
+      <div className='explore-container'>
         <div className='explore-text-container explore-item'>
           <div className='explore-text-btn'>
-            <h1>Design your dream home with the perfect furniture.</h1>
+            <h1>Design your dream home with <br/>the perfect furniture.</h1>
             <p>We are making it easy for you to find your perfect match.</p>
             <button className='explore-btn'>Explore All Product</button>
           </div>
         </div>
         <div className='explore-img-container explore-item'>
-          <div className='explore-img-bg' />
+          {/* <div className='explore-img-bg' /> */}
           <div className='explore-img'>
             <img src={explore} alt='explore-img' />
           </div>
