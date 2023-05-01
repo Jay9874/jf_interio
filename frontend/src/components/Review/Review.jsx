ae4d7ede0daff73085b27c19346bd0f8888dbe32
Jay Prakash Sharma <jayprakashsharma225@gmail.com>
2023-05-01 23:03:54 +0530

    Update Review.jsx
    removed section padding

--- frontend/src/components/Review/Review.jsx
+++ frontend/src/components/Review/Review.jsx
@@ -38,7 +38,7 @@ export default function Review () {
   return (
     <div className='review-section'>
       <div className='review-container '>
-        <div className='review-header-container section-padding'>
+        <div className='review-header-container'>
           <h1>Our Reviews</h1>
         </div>
         <div className='review-card-container'>
