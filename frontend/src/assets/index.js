5f7320de5520bf8b0e467a36a15780a8db44be8f
Jay9874 <jayprakashsharma225@gmail.com>
2023-05-12 21:41:52 +0530

    increased performance

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,15 +1,15 @@
 // Home pagae images
 export { default as cat_chair } from './cat_chair.png'
-export { default as cat_sofa } from './cat_sofa.png'
+export { default as cat_sofa } from './cat_sofa.webp'
 export { default as cat_cabinet } from './cat_cabinet.png'
 export { default as cat_lamp } from './cat_lamp.png'
-export { default as sofa_chair } from './sofa_chair.jpg'
+export { default as sofa_chair } from './sofa_chair.webp'
 export { default as check } from './check.png'
-export { default as landing_img } from './landing_page.jpg'
-export { default as explore } from './explore.svg'
+export { default as landing_img } from './landing_page.webp'
+export { default as explore } from './explore.webp'
 export { default as App_Store_Badge } from './App_Store_Badge.png'
 export { default as Google_Play_Badge } from './Google_Play_Badge.png'
-export { default as offer_bg } from './offer_bg.jpg'
+export { default as offer_bg } from './offer_bg.webp'
 export { default as cat_bed } from './cat_bed.png'
 
 // category images
