64ef769fbf2d90812f7861084c72cee1f788afff
Jay9874 <jayprakashsharma225@gmail.com>
2023-05-26 17:56:59 +0530

    complete animation

--- frontend/src/components/Auth/Auth.jsx
+++ frontend/src/components/Auth/Auth.jsx
@@ -26,54 +26,83 @@ export default function Auth ({ hideAuthForm }) {
     }, [ref])
   }
 
+  function switchToLogin () {
+    setLogin(true)
+    setAuthHeading('Welcome Back')
+    setAuthButtonText('Login')
+  }
+
+  function switchToSignup () {
+    setLogin(false)
+    setAuthHeading("Let's start afresh")
+    setAuthButtonText('Create')
+  }
+
   return (
     <div className='auth-form-popup' ref={authFormRef}>
-      <div className='auth-shadow-container'>
-        <div className='auth-content-container'>
-          <div className='auth-form-container' ref={wrapperRef}>
-            <div className='auth-form-main'>
-              <div className='auth-form-switcher'>
-                <div className='auth-form-switcher-btns form-switch-login'>
-                  <button
-                    className={
-                      login
-                        ? 'auth-form-switcher-btn active'
-                        : 'auth-form-switcher-btn'
-                    }
-                    onClick={() => setLogin(true)}
-                  >
-                    Login
-                  </button>
-                </div>
-                <div className='auth-form-switcher-btns form-switch-signup'>
-                  <button
-                    className={
-                      !login
-                        ? 'auth-form-switcher-btn active'
-                        : 'auth-form-switcher-btn'
-                    }
-                    onClick={() => setLogin(false)}
-                  >
-                    New Account
-                  </button>
-                </div>
+      <div className='auth-shadow-container'></div>
+      <div className='auth-content-container'>
+        <div className='auth-form-container' ref={wrapperRef}>
+          <div className='auth-form-main'>
+            <div className='auth-form-switcher'>
+              <div
+                className={`auth-form-switcher-btns form-switch-login ${
+                  login ? 'colored' : 'active'
+                }`}
+                onClick={switchToLogin}
+              >
+                <button className='auth-form-switcher-btn'>Login</button>
               </div>
+              <div
+                className={`auth-form-switcher-btns form-switch-signup ${
+                  login ? 'active' : 'colored'
+                }`}
+                onClick={switchToSignup}
+              >
+                <button className='auth-form-switcher-btn'>New Account</button>
+              </div>
+            </div>
 
-              <form className='auth-form' autoComplete='off'>
-                <div className='auth-form-heading-container'>
-                  <h1 className='auth-form-heading'>{authHeading}</h1>
-                </div>
-                <div className='auth-input-container'>
-                  <div className='auth-input-item auth-email-container'>
-                    <label htmlFor='email'>Email</label>
+            <form className='auth-form' autoComplete='off'>
+              <div className='auth-form-heading-container'>
+                <h1 className='auth-form-heading'>{authHeading}</h1>
+              </div>
+              <div className='auth-input-container'>
+                {!login && (
+                  <div className='auth-input-item auth-fullname-container'>
+                    <label htmlFor='fullname'>Full Name</label>
                     <input
                       autoComplete='off'
                       className='auth-form-inputs'
-                      type='email'
-                      id='email'
-                      placeholder='email@example.com'
+                      type='text'
+                      id='fullname'
+                      placeholder='Ansh Sharma'
                     />
                   </div>
+                )}
+                {!login && (
+                  <div className='auth-input-item auth-username-container'>
+                    <label htmlFor='username'>User Name</label>
+                    <input
+                      autoComplete='off'
+                      className='auth-form-inputs'
+                      type='text'
+                      id='username'
+                      placeholder='anshsharma'
+                    />
+                  </div>
+                )}
+                <div className='auth-input-item auth-email-container'>
+                  <label htmlFor='email'>Email</label>
+                  <input
+                    autoComplete='off'
+                    className='auth-form-inputs'
+                    type='email'
+                    id='email'
+                    placeholder='email@example.com'
+                  />
+                </div>
+                {login && (
                   <div className='auth-input-item auth-password-container'>
                     <label htmlFor='password'>Password</label>
                     <input
@@ -83,14 +112,36 @@ export default function Auth ({ hideAuthForm }) {
                       placeholder='~N}ypepD6^5(U;~B'
                     />
                   </div>
-                </div>
-                <div className='auth-btn-container'>
-                  <button className='auth-form-btn' type='submit'>
-                    {authButtonText}
-                  </button>
-                </div>
-              </form>
-            </div>
+                )}
+                {!login && (
+                  <div className='auth-input-item auth-password-container'>
+                    <label htmlFor='password'>New Password</label>
+                    <input
+                      className='auth-form-inputs'
+                      type='password'
+                      id='password'
+                      placeholder='~N}ypepD6^5(U;~B'
+                    />
+                  </div>
+                )}
+                {!login && (
+                  <div className='auth-input-item auth-newpassword-container'>
+                    <label htmlFor='new-password'>Repeat Password</label>
+                    <input
+                      className='auth-form-inputs'
+                      type='password'
+                      id='new-password'
+                      placeholder='~N}ypepD6^5(U;~B'
+                    />
+                  </div>
+                )}
+              </div>
+              <div className='auth-btn-container'>
+                <button className='auth-form-btn' type='submit'>
+                  {authButtonText}
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
