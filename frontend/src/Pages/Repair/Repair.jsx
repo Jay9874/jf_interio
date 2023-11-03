07c599cf7dda31becf806cef4137c20c85846c70
Jay9874 <jayprakashsharma225@gmail.com>
2023-11-03 23:58:16 +0530

    repair section full screen

--- frontend/src/Pages/Repair/Repair.jsx
+++ frontend/src/Pages/Repair/Repair.jsx
@@ -3,7 +3,7 @@ import './repair.css'
 
 export default function Repair() {
   return (
-    <div>
+    <div className='repair-container'>
         <h1>Welcome to repair section</h1>
         <p>We are working on it.</p>
     </div>
