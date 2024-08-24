f1ea31d95c304420b78faba5afa4e8e68845a300
Jay9874 <jayprakashsharma225@gmail.com>
2024-08-24 20:19:47 +0530

    upgrade packages

--- config/db.js
+++ config/db.js
@@ -1,19 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 // Connect to the database
-const db = process.env.DB_URI;
+const db = process.env.DB_URI
 const connectDB = async () => {
-    try{
-        mongoose.set('strictQuery', false);
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        console.log('Database connected!!');
-    }catch(err){
-        console.log(`Ohh...error occured, ${err.message}`);
-        process.exit(1);
-    }
+  try {
+    mongoose.set('strictQuery', false)
+    await mongoose.connect(db)
+    console.log('Database connected!!')
+  } catch (err) {
+    console.log(`Ohh...error occured, ${err.message}`)
+    process.exit(1)
+  }
 }
 
-module.exports = connectDB;
+module.exports = connectDB
